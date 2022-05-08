1ec41b10024f5bd11130845c9f5afcba13d07fbe
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2022-05-08 09:55:07 +0300

    project folders

---
+++ router/auth.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
+  logout,
+} = require('../controllers/auth');
+
+const router = express.Router();
+const { protect } = require('../middleware/auth');
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.get('/me', protect, getMe);
+router.get('/logout', protect, logout);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileupload = require('express-fileupload');
+const mongoSanitize = require('express-mongo-sanitize');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+// Route files
+const bootcamps = require('./router/bootcamps');
+const courses = require('./router/courses');
+const auth = require('./router/auth');
+const users = require('./router/users');
+const reviews = require('./router/reviews');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  // app.use(logger);
+}
+
+// File uploading
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// set security headers
+app.use(helmet());
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limitting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
+app.use(limiter);
+
+// Enable CORS
+app.use(cors());
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
